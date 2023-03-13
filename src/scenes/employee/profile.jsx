bb93f5e8ffef163864b1021d77babd59fbc1f9f6
Jeyaroshini L S S <72503637+Jeyaroshini@users.noreply.github.com>
2023-03-13 10:31:37 +0530

    Employee

---
+++ src/scenes/employee/profile.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import UserCards from "./userCards"
+import "./styles.css";
+const Profile = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <UserCards />
+      </div>
+    </div>
+  )
+}
+export default Profile
